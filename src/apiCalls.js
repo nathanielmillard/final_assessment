7a03f354e28a33282d697cffc7ff578199f717a7
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-10 12:03:26 -0700

    Add id to POST and write delete function.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,13 +7,14 @@ export const getUrls = () => {
       .catch(error => console.log(error))
 }
 
-export const postUrl = (longURL, title) => {
+export const postUrl = (id, longURL, title) => {
   return fetch('http://localhost:3001/api/v1/urls', {
     method: 'POST',
     headers: {
     'Content-Type': 'application/json',
     },
     body: JSON.stringify({
+      "id": id,
       "long_url": longURL,
       "title": title
     })
@@ -21,4 +22,21 @@ export const postUrl = (longURL, title) => {
   .then(response => response.json())
   .then(response => console.log('success', response))
   .catch(error => console.log(error))
+}
+
+export const deleteUrl = (id) => {
+  let url = `http://localhost:3001/api/v1/urls/${id}`
+  return ( fetch(url, {
+    method: 'DELETE',
+  })
+  .then(response => {
+    if(response.ok){
+      console.log('success!')
+    }
+  }
+  )
+  .catch(error => {
+    console.log(error)
+  })
+)
 }
