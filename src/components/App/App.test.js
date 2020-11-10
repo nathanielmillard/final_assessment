41baf1c92d7ca3e6479fbc2627bc0e651b08d6e3
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-10 12:03:55 -0700

    Fix test.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom'
 import {render, screen, waitFor} from '@testing-Library/react'
 import App from './App.js'
 import userEvent from '@testing-library/user-event'
-import {getUrls, postUrl} from '../../apiCalls';
+import {getUrls, postUrl, deleteUrl} from '../../apiCalls';
 jest.mock('../../apiCalls')
 
 describe('App', ()=> {
@@ -54,7 +54,7 @@ describe('App', ()=> {
     expect(titleInput.value).toBe('google')
     userEvent.type(urlInput, 'www.google.com')
     expect(urlInput.value).toBe('www.google.com')
-    let button = screen.getByRole('button')
+    let button = screen.getByText('Shorten Please!')
     userEvent.click(button)
     let secondTitle = await waitFor(()=> {
       return screen.getByText("google")
@@ -62,5 +62,37 @@ describe('App', ()=> {
     let secondLink = screen.getByText("http://localhost:3001/useshorturl/3")
     expect(secondTitle).toBeInTheDocument();
     expect(secondLink).toBeInTheDocument();
+  })
+  it('Should allow a user to delete an entry', async () => {
+    getUrls.mockResolvedValueOnce([{long_url: "https://stackoverflow.com/questions/30142361/react…t-typeerror-this-props-data-map-is-not-a-function", title: "Stack Overflow", id: 2, short_url: "http://localhost:3001/useshorturl/2"}])
+    getUrls.mockResolvedValueOnce([{long_url: "https://stackoverflow.com/questions/30142361/react…t-typeerror-this-props-data-map-is-not-a-function", title: "Stack Overflow", id: 2, short_url: "http://localhost:3001/useshorturl/2"}, {long_url: "www.google.com", title: "google", id: 3, short_url: "http://localhost:3001/useshorturl/3"}])
+    getUrls.mockResolvedValueOnce([{long_url: "https://stackoverflow.com/questions/30142361/react…t-typeerror-this-props-data-map-is-not-a-function", title: "Stack Overflow", id: 2, short_url: "http://localhost:3001/useshorturl/2"}])
+    render(
+      <App
+      />
+    )
+    let title = await waitFor(()=> {
+      return screen.getByText("Stack Overflow")
+    })
+    let shortLink = screen.getByRole('link')
+    let longUrl = screen.getByText('https://stackoverflow.com/questions/30142361/react…t-typeerror-this-props-data-map-is-not-a-function')
+    expect(title).toBeInTheDocument()
+    let titleInput = screen.getByPlaceholderText('Title...')
+    let urlInput = screen.getByPlaceholderText('URL to Shorten...')
+    userEvent.type(titleInput, 'google')
+    expect(titleInput.value).toBe('google')
+    userEvent.type(urlInput, 'www.google.com')
+    expect(urlInput.value).toBe('www.google.com')
+    let button = screen.getByText('Shorten Please!')
+    userEvent.click(button)
+    let secondTitle = await waitFor(()=> {
+      return screen.getByText("google")
+    })
+    let secondLink = screen.getByText("http://localhost:3001/useshorturl/3")
+    expect(secondTitle).toBeInTheDocument();
+    expect(secondLink).toBeInTheDocument();
+    let deleteUrlBttn = screen.getAllByAltText('Delete')
+    userEvent.click(deleteUrlBttn[1])
+    expect(deleteUrl).toHaveBeenCalledWith("3")
   })
 })
